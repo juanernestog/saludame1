99086772a273ffac985b25a6ce49246dc65ef14d
JuanE <juanernestogranados@gmail.com>
2022-04-10 18:21:36 -0500

    Update MIR implementation od code

--- app2.js
+++ app2.js
@@ -1,4 +1,4 @@
-//MIR implementation
+//MIR implementation of saludame 1
 const express = require('express');
 const app = express();
 
@@ -7,6 +7,19 @@ app.get('/', (req, res) => {
   if (!name || name.length === 0) {
     name = 'desconocido';
   }
+
+  res.send(`<h1>Hola ${name}!</h1>`);
+});
+
+app.listen(3000, () => console.log('Listening on port 3000!'));
+
+// MIR implementation of Saludame2
+const express = require('express');
+const app = express();
+
+app.get('/makers/:name', (req, res) => {
+  let name = req.params.name;
+  name = name.replace(/^\w/, (c) => c.toUpperCase());
 
   res.send(`<h1>Hola ${name}!</h1>`);
 });

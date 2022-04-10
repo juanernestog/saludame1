4f2786243acdba44e198cdc42671b7df83dfe3a6
JuanE <juanernestogranados@gmail.com>
2022-04-10 16:00:28 -0500

    MIT solution added as app2.js

---
+++ app2.js
@@ -0,0 +1,14 @@
+//MIR implementation
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  let name = req.query.nombre;
+  if (!name || name.length === 0) {
+    name = 'desconocido';
+  }
+
+  res.send(`<h1>Hola ${name}!</h1>`);
+});
+
+app.listen(3000, () => console.log('Listening on port 3000!'));
